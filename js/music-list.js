274bb5cc5cadd489f2eac208845706c29ad9dc44
Prakash Kumar gupta <109419191+anantprakashgupta@users.noreply.github.com>
2023-01-04 11:48:15 +0530

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,45 @@
+
+let allMusic = [
+  {
+    name: "chehra kya dekhte ho",
+    artist: "Kumar sanu",
+    img: "song1",
+    src: "song1"
+  },
+  {
+    name: "Dil Diwane Ka Dola",
+    artist: "Kumar sanu",
+    img: "song2",
+    src: "song2"
+  },
+  {
+    name: "Dil Pardeshi Ho Gya",
+    artist: "Kumar sanu",
+    img: "song3",
+    src: "song3"
+  },
+  {
+    name: "Ek Ladki Ko dekh To",
+    artist: "Kumar sanu",
+    img: "song4",
+    src: "song4"
+  },
+  {
+    name: "Ek Sanam chahiye ",
+    artist: "Kumar sanu",
+    img: "song5",
+    src: "song5"
+  },
+  {
+    name: "Itna Mein Chahu Tujhe",
+    artist: "Kumar sanu",
+    img: "song6",
+    src: "song6"
+  },
+  {
+    name: "Payaliya",
+    artist: "Kumar sanu",
+    img: "song7",
+    src: "song7"
+  },
+];
